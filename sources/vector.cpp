0d730dff440dc7aab8c0580e094f177121313f11
Lobay <32384708+Lobay@users.noreply.github.com>
2018-03-23 23:44:25 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -6,110 +6,118 @@
 vector_t::vector_t()
 {
 	elements_ = nullptr;
-	size_ = 0;
-	capacity_ = 0;
+	size_=0;
+	capacity_ =0;
 }
 
 vector_t::vector_t(vector_t const & other)
 {
-	size_ = other.size_;
+	size_= other.size_;
 	capacity_ = other.capacity_;
 	elements_ = new int [capacity_];
-	for (unsigned int i=0; i < size_; i++)
-		elements_[i] = other.elements_[i];
+	for (unsigned int i=0; i< size_; i++) {
+		elements_[i] =other.elements_[i];
+	}
 }
 
-vector_t& vector_t::operator=(vector_t const& other) 
+vector_t & vector_t::operator =(vector_t const & other)
 {
-	if (this != &other) {
-		delete[] elements_;
-		this->size_ = other.size_;
-    	this->capacity_ = other.capacity_;
-    	this->elements_ = new int[capacity_];
-    	for (unsigned int i = 0; i < size_; i++) elements_[i] = other.elements_[i];
+	if (this->elements_==other.elements_) return *this;
+	else {
+		delete [] elements_;
+		size_= other.size_;
+	capacity_ = other.capacity_;
+	elements_ = new int [capacity_];
+	for (unsigned int i=0; i< size_; i++) {
+		elements_[i] =other.elements_[i];
+	}
 	}
 	return *this;
 }
 
 bool vector_t::operator ==(vector_t const & other) const
 {
-	if (this -> size_ == other.size_) {
-		for (unsigned int i = 0; i < size_; i++){
-		 if (this->elements_[i] != other.elements_[i]) {
-        return false;
-        break;
-      }
-    }
-    return true;
-  } else
+	if (this->size_ == other.size_) {
+		for (int i = 0; i < size_; i++) {
+			if (this->elements_[i] != other.elements_[i]) {
+        		return false;
+        		break;
+    		}
+    	}
+    	return true;
+	} 
+	else
     return false;
 }
 
 vector_t::~vector_t()
 {
+	if (elements_!= nullptr)
 	delete [] elements_;
-	size_ = 0;
-	capacity_ = 0;
+	size_=0;
+	capacity_=0;
 }
 
 std::size_t vector_t::size() const
 {
     return size_;
-    return 0;
 }
 
 std::size_t vector_t::capacity() const
 {
     return capacity_;
-    return 0;
 }
 
 void vector_t::push_back(int value)
 {
-	if (size_ == 0){
-		size_ = 1;
-		capacity_ = 1;
-		elements_ = new int [capacity_];
-		elements_[0] = value;
+	if ((capacity_ == size_ ) && (size_ !=0))
+	{ int *elements_copy= new int [capacity_*2];
+	capacity_= capacity_*2;
+	for (unsigned int i=0; i< size_; i++)
+		elements_copy[i] =elements_[i];
+	size_++;
+	elements_copy[size_-1] = value;
+	delete [] elements_;
+	elements_ = new int [capacity_];
+	for (unsigned int i=0; i< size_; i++)
+		elements_[i] =elements_copy[i];
+	delete [] elements_copy;
+	return;
+	};
+	if (size_ == 0) {
+		size_=1;
+		elements_ = new int [1];
+		capacity_=1;
+		elements_[0]=value;
+		return;
 	}
-	else if (size_ == capacity_)
-	{
-		capacity_ = capacity_ *2;
-		int* elements1 = new int [capacity_];
-		for(unsigned int i = 0; i < size_; i++)
-			elements1[i] = elements_[i];
-		delete [] elements_;
-		elements_[size_] = value;
-		size_++;
-	}
-	else {
-		elements_[size_] = value;
-		size_++;
-	}
+	size_++;
+	elements_ [size_-1] = value;
 }
 
 void vector_t::pop_back()
 {
-	size_ --;
-	if (size_ == 0 || size_ * 4 == capacity_){
-		int* elements1;
-		elements1 = new int [size_];
-		for (unsigned int i = 0; i < size_; i++)
-			elements1[i] = elements_[i];
-	delete[] elements_;
-	capacity_ = capacity_ / 2;
-	elements_ = new int[capacity_];
-	for (unsigned int i = 0; i < size_; i++)
-		elements_[i] = elements1[i];
-	delete[] elements1;
-}
+	if (size_==0) return;
+	if (size_==1) {size_=0; capacity_=1; return;};
+	size_--;
+	if (capacity_==(4*size_)) {
+		int *elements_copy= new int [capacity_/2];
+	capacity_= capacity_/2;
+	for (unsigned int i=0; i< size_; i++)
+		elements_copy[i] =elements_[i];
+		delete [] elements_;
+		elements_= new int [capacity_];
+		for (unsigned int i=0; i< size_; i++)
+		elements_[i] =elements_copy[i];
+	delete [] elements_copy;
+	return; }
 }
 
 int & vector_t::operator [](std::size_t index)
 {
 	return elements_[index];
 }
- 
+
 int vector_t::operator [](std::size_t index) const
 {
 	return elements_[index];
